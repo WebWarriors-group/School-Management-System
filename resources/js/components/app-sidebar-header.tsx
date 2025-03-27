f5a5896d6792816343ddd0bb57a135816cd7ef94
Piyumantha Munasinghe <piyumanthajm@gmail.com>
2025-03-27 14:29:29 +0530

    Frontend update - Jayfa_CSC/002
    Co-authored-by: JayfaAnjum <jayfatha62@gmail.com>

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,14 +1,22 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
+import { NavUser } from '@/components/nav-user';
+
 
 export function AppSidebarHeader({ breadcrumbs = [] }: { breadcrumbs?: BreadcrumbItemType[] }) {
     return (
-        <header className="border-sidebar-border/50 flex h-16 shrink-0  items-center gap-2 border-b px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
-            <div className="flex items-center gap-2 bg-blue-100">
-                <SidebarTrigger className="-ml-1" />
-                <Breadcrumbs breadcrumbs={breadcrumbs} />
-            </div>
-        </header>
+        <header className=" bg-white sticky top-0 border-sidebar-border/50 flex h-16 shrink-0 items-center gap-2 border-b px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
+  <div className="flex items-center gap-2 ">
+    <SidebarTrigger className="-ml-1" />
+    {/* <Breadcrumbs breadcrumbs={breadcrumbs} /> */}
+    Toggle Screen
+  </div>
+
+  <div className="fixed ml-250 right-4 flex items-center gap-2">
+    <NavUser />
+  </div>
+</header>
+
     );
 }
