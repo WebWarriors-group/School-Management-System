82e634b042b5f3cb25f09dde8fd84ba69c4762f6
Piyumantha Munasinghe <piyumanthajm@gmail.com>
2025-04-12 15:47:50 +0530

    User management, Admin dashboard frontend improved - Jayfa_CSC/002

--- resources/js/pages/homepage.tsx
+++ resources/js/pages/homepage.tsx
@@ -36,9 +36,9 @@ export default function Navbar() {
             <Head title="Mahadivulwewa National School" />
             <div className="w-full">
                 {/* Main Navbar */}
-                <nav className="bg-[#800000] py-2 text-white shadow-md w-full">
-                    <div className="w-full flex items-center px-5 justify-between">
-                        {/* Logo and School Name */}
+                <nav className="bg-[#4B0000] py-2 text-white shadow-md w-full">
+                    <div className="w-full flex items-center  px-5">
+                        {/* Logo */}
                         <div className="flex items-center">
                             <img src="images/School.jpg" alt="Logo" className="h-14 w-14 rounded-full ml-[-10px]" />
                             <span className="font-[Orbitron] text-lg font-semibold text-[#FDDEBD] ml-3 whitespace-nowrap">
@@ -47,10 +47,7 @@ export default function Navbar() {
                         </div>
 
                         {/* Login Button */}
-                        <Link
-                            href={route('login')}
-                            className="hidden md:inline-block rounded-2xl bg-white px-10 py-2 text-sm font-medium text-black transition duration-300 hover:bg-blue-700 hover:text-white"
-                        >
+                        <Link href={route('login')} className="hidden ml-230 md:inline-block rounded-2xl bg-white px-10 py-2 text-sm font-medium text-black transition duration-300 hover:bg-gradient-to-r from-orange-600 to-yellow-500 hover:text-white">
                             Login
                         </Link>
 
@@ -96,13 +93,13 @@ export default function Navbar() {
                 <section className="flex h-[80vh] w-full items-center justify-center bg-white text-center text-black">
                     <div className="container px-4">
                         <img src="images/School.jpg" alt="Logo" className="mx-auto mt-9 h-100 w-109" />
-                        <h1 className="text-4xl font-bold text-[#800000]">මහදිවුල්වැව මහ විද්‍යාලය</h1>
-                        <h1 className="text-4xl font-bold text-[#800000]">Mahadivulwewa National School</h1>
+                        <h1 className="text-4xl font-bold text-[#4B0000]">මහදිවුල්වැව මහ විද්‍යාලය</h1>
+                        <h1 className="text-4xl font-bold text-[#4B0000]"> T / Tn / Mahadivulwewa  National School</h1>
                     </div>
                 </section>
 
                 {/* Footer Section */}
-                <section className="bg-[#800000]   text-white">
+                <section className="bg-[#4B0000]   text-white">
     <div className="container mx-auto px-6 py-6 grid grid-cols-1 md:grid-cols-4 gap-8">
         {/* Column 1 */}
         <div>
@@ -147,14 +144,14 @@ export default function Navbar() {
                 <a href="#" className="hover:text-gray-300"><Mail size={30} /></a>
                 {/* <a href="#" className="hover:text-gray-300"><YouTube size={24} /></a> */}
             </div>
-           <h3>077 879 2078</h3>
+           <h2>Tel :+9477 879 2078</h2>
         </div>
     </div>
 
     <div className="border-t border-gray-500 mt-8 bg-white"></div>
 
     {/* Footer Bottom */}
-    <div className="container mx-auto px-6 py-6 text-center text-[17px] text-gray-600 bg-gray-200">
+    <div className="container mx-auto px-6 py-5 text-center text-[17px] text-gray-600 bg-gray-200">
         <p>&copy; {new Date().getFullYear()} Mahadivulwewa Mahavidyalaya. All rights reserved.</p>
     </div>
 </section>
