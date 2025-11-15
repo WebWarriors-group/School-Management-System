e72a61e45c080cc0561aa0a16be54a46dc27a792
Kaveesha Dilshani <kaveedill2000@gmail.com>
2025-11-15 17:12:20 +0530

    Update StudentAcademic.php

--- app/Models/StudentAcademic.php
+++ app/Models/StudentAcademic.php
@@ -39,7 +39,6 @@ class StudentAcademic extends Model
 
     protected $appends = ['scholarship_status'];
 
-<<<<<<< HEAD
 
     protected static function booted()
 {
@@ -64,8 +63,6 @@ class StudentAcademic extends Model
     });
 }
 
-=======
->>>>>>> da966e3c28a260bde879823d91e77d7cba4e1f12
     public function getScholarshipStatusAttribute(){
         $statuses=[];
 
